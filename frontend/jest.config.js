eb82e5853233e120b3bbdfa4ec8556d37ee6e683
selim <selimdev00@gmail.com>
2023-04-07 09:41:50 +0500

    [chores]: preparing test environmet & removed default counter feature

---
+++ frontend/jest.config.js
@@ -0,0 +1,15 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  extensionsToTreatAsEsm: ['.ts'],
+  esModules: true,
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+};
--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import pokemonReducer from '../features/pokemon/pokemonSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     pokemon: pokemonReducer,
   },
 });
